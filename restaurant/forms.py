5d10b9b0a18789d8e616a2890317806118d62779
Henry Leon <henryatusabes@gmail.com>
2022-11-21 22:31:53 +0000

    Update Views.py, forms.py and urls.py to wired up the site

---
+++ restaurant/forms.py
@@ -0,0 +1,45 @@
+from django.forms import ModelForm
+from django.core import validators
+from django import forms
+from .models import Booking
+
+
+class OnlineForm(ModelForm):
+    """
+    This form is connected with the view
+    in order to provide users with the neccessary
+    fields for making a booking
+    It also provides the labels and placeholder
+    text for each field, as wells as the widgets
+    and handles validation where required.
+    """
+    name = forms.CharField(
+        label='Booking Name',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Booking Name'}),
+    )
+
+    email_address = forms.EmailField(
+        label='Email Address',
+        required=True,
+        validators=[validators.EmailValidator(message="Invalid Email")],
+        widget=forms.TextInput(attrs={'placeholder': 'Email Address'}),
+    )
+
+    phone = forms.IntegerField(
+        label='Contact Number',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Contact number'}),
+    )
+
+    class Meta:
+        """Defines which model to pull the
+        fields from"""
+        model = Booking
+        # Tell the form to use all the fields provided
+        fields = '__all__'
+        # Except fot the user field
+        exclude = ('user', )
+        widgets = {
+            'date': forms.DateInput()
+        }
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,13 +4,13 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    # path('booking/', views.BookingView.as_view(), name='booking'),
+    path('booking/', views.BookingView.as_view(), name='booking'),
     path('menu/', views.Menu.as_view(), name='menu'),
-    # path(
-    #     'manage_bookings/',
-    #     views.ListBookingView.as_view(), name='manage_bookings'),
-    # path(
-    #     'update_booking/<booking_id>',
-    #     views.update_booking_view, name='update'),
-    # path('delete_booking/<booking_id>', views.delete_booking, name='delete'),
+    path(
+        'manage_bookings/',
+        views.ListBookingView.as_view(), name='manage_bookings'),
+    path(
+        'update_booking/<booking_id>',
+        views.update_booking_view, name='update'),
+    path('delete_booking/<booking_id>', views.delete_booking, name='delete'),
 ]
