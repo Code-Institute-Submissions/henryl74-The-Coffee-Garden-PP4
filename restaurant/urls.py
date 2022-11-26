d98e58f1c75b5bd076624e623ae88de66749e984
Henry Leon <henryatusabes@gmail.com>
2022-11-26 19:27:28 +0000

    Update path on urls.py file to be able to update the booking

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('booking/', views.BookingView.as_view(), name='booking'),
     path('menu/', views.Menu.as_view(), name='menu'),
-    path(
-        'manage_bookings/',
-        views.ListBookingView.as_view(), name='manage_bookings'),
+    # path(
+    #     'manage_bookings/',
+    #     views.ListBookingView.as_view(), name='manage_bookings'),
     path('thank_you/', views.ThankYou.as_view(), name='thank_you'),
     path('update_booking/', views.ListBookingView.as_view(), name='update_booking'),
-    path('edit_bookings/<booking_id>', views.edit_booking_view, name='edit'),
+    path('manage_bookings/<booking_id>', views.edit_booking_view, name='edit'),
     path('delete_booking/<booking_id>', views.delete_booking, name='delete'),
 ]
