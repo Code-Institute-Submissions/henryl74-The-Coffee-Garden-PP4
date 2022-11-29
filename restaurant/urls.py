e76118d1236ebad87c7c31d8272538b9cb655979
Henry Leon <henryatusabes@gmail.com>
2022-11-29 20:53:29 +0000

    Update booking path under urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     #     'manage_bookings/',
     #     views.ListBookingView.as_view(), name='manage_bookings'),
     path('thank_you/', views.ThankYou.as_view(), name='thank_you'),
-    path('update_booking/', views.ListBookingView.as_view(), name='update_booking'),
+    path(
+        'update_booking/',
+        views.ListBookingView.as_view(), name='update_booking'),
     path('manage_bookings/<booking_id>', views.edit_booking_view, name='edit'),
     path('delete_booking/<booking_id>', views.delete_booking, name='delete'),
 ]
