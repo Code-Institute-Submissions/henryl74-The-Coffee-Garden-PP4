5647bec7e01b22321d4fdc48cf022463c00ad189
Henry Leon <henryatusabes@gmail.com>
2022-12-01 23:19:21 +0000

    Tidy up forms.py file

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -7,17 +7,17 @@ from .models import Booking
 class DateInput(forms.DateInput):
     """
     This class provides a widget for use in the
-    booking form. It provides a calendar for users
-    to pick the booking date from
+    booking form. It cretes a better UX when choosing the date
+    for the booking.
     """
     input_type = 'date'
 
 
 class OnlineForm(ModelForm):
     """
-    This form is connected with the view
+    This class is connected with the view
     in order to provide users with the neccessary
-    fields for making a booking
+    fields for making a booking.
     It also provides the labels and placeholder
     text for each field, as wells as the widgets
     and handles validation where required.
@@ -35,12 +35,12 @@ class OnlineForm(ModelForm):
     )
 
     class Meta:
-        """Defines which model to pull the
-        fields from"""
+        """
+        This class defines which model to pull the
+        fields from.
+        """
         model = Booking
-        # Tell the form to use all the fields provided
         fields = '__all__'
-        # Except fot the user field
         exclude = ('user', )
         widgets = {
             'date': DateInput()
